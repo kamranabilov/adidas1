9b960b519f83cba4193820f9c67480f7a2fcc4b8
kamranabilov <kamrangab@code.edu.az>
2022-05-12 07:57:38 +0400

    added adidas2

--- Adidas.js/assets/js/main.js
+++ Adidas.js/assets/js/main.js
@@ -14,3 +14,39 @@
 //         }
 //     }
 // })
+
+$(document).ready(function(){
+    // $(".owl-carousel").owlCarousel();
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+  });
+
+
+let newMessage = false;
+setInterval(() =>{
+    if(newMessage){
+        document.title="(1)Have You New Message"
+
+        newMessage = !newMessage;
+    }else{
+        document.title = "Instgram"
+
+        newMessage = !newMessage; 
+    }
+}, 1000);       
